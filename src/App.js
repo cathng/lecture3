25c31582df48a4f883dd844fd3aec4ace173661b
Yazan Alaboudi <yazan.alaboudi@gmail.com>
2018-03-29 01:05:06 -0400

    lecture 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import { mockTimeline, Timeline } from './components';
+import { Timeline } from './components';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Timeline {...mockTimeline}/>
+        <Timeline />
       </div>
     );
   }
